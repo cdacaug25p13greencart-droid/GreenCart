aca5f58be45108a3fa09388eb0dfac87fdc090e9
Sachin Lokhande <sachinlokhande4154@gmail.com>
2026-01-23 15:32:20 +0530

    Remove Greencart_Product_Service folder

--- backend/Greencart_Product_Service/backend/GreenCart_User_Service/src/main/java/com/greencart/repositories/SecurityQuestionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.greencart.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greencart.entities.SecurityQuestion;
-
-public interface SecurityQuestionRepo extends JpaRepository<SecurityQuestion, Integer> {
-
-}
